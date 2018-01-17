e3fa83a5172fbf361406a90eef02791ef493f519
FungSaikit <675703302@qq.com>
2018-01-17 20:41:05 +0800

    增加了放大缩小格子的功能

--- JavaScript/fung.js
+++ JavaScript/fung.js
@@ -1,10 +1,12 @@
 var editMode = false;
 var autoCount = false;
+var defaultSize = 24;
 
 var rowsInput = document.getElementById('row_input');
 var columnsInput = document.getElementById('column_input');
 var cellsTable = document.getElementById('cells_table');
 var style = document.getElementById('current_style');
+var cellSize = document.getElementById('current_size');
 var selectedCellElement = document.getElementById('selected_cells');
 var selectedCellsNumber = document.getElementById('selected_number');
 var allCells;
@@ -19,13 +21,13 @@ function useAutoCount(object) {
 }
 
 function enterListener() {
-    if(event.keyCode == 13) {
+    if (event.keyCode == 13) {
         createCells();
     }
 }
 
 function countListener() {
-    if(event.keyCode == 13) {
+    if (event.keyCode == 13) {
         countCells();
     }
 }
@@ -62,14 +64,14 @@ function createCells() {
     var tableContent = ''
     tableContent += '<tr>';
     for (var i = 0; i <= columns; i++) {
-        tableContent += '<td>' + i + '</td>'
+        tableContent += '<td class="rnc_num">' + i + '</td>'
     }
     tableContent += '</tr>';
 
     for (var i = 0; i < rows; i++) {
-        tableContent += '<tr><td>' + (i + 1) + '</td>';
+        tableContent += '<tr><td class="rnc_num">' + (i + 1) + '</td>';
         for (var j = 0; j < columns; j++) {
-            tableContent += '<td class="cell" row="' + (i+1) + '" column="' + (j+1) + '" onmouseenter="mouseHoverCells(this)" onmousedown="turnOnEditMode(this)" ></td>'
+            tableContent += '<td class="cell" row="' + (i + 1) + '" column="' + (j + 1) + '" onmouseenter="mouseHoverCells(this)" onmousedown="turnOnEditMode(this)" ></td>'
         }
         tableContent += '</tr>';
     }
@@ -96,4 +98,21 @@ function countCells() {
 
 function changeSelectedColor(object) {
     style.innerHTML = '.selected_cell {background-color: ' + object.value + ';}'
+}
+
+var zoom = {
+    out: function () {
+        defaultSize = defaultSize/4 > 1 ? Math.ceil(defaultSize/2) : defaultSize;
+        cellSize.innerHTML = '#cells_table td { width: ' + defaultSize + 'px; min-width: ' + defaultSize + 'px; max-width: ' + defaultSize + 'px; height: ' + defaultSize + 'px; max-height: ' + defaultSize + 'px; min-height: ' + defaultSize + 'px; line-height: ' + defaultSize + 'px; font-size: ' + defaultSize/2 + 'px;  }';
+        if (defaultSize < 24) {
+            cellSize.innerHTML += '.rnc_num { display: none; }'
+        }
+    },
+    in: function () {
+        defaultSize *= 2;
+        cellSize.innerHTML = '#cells_table td { width: ' + defaultSize + 'px; min-width: ' + defaultSize + 'px; max-width: ' + defaultSize + 'px; height: ' + defaultSize + 'px; max-height: ' + defaultSize + 'px; min-height: ' + defaultSize + 'px; line-height: ' + defaultSize + 'px; font-size: ' + defaultSize/2 + 'px;  }';
+        if (defaultSize < 24) {
+            cellSize.innerHTML += '.rnc_num { display: none; }'
+        }
+    }
 }
